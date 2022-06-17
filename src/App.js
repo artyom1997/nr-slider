8fd17ab3cf6b3a39ea7a2cbaf70e27378684e6b2
Artyom <artyommalxasyan97@gmail.com>
2022-06-17 13:55:16 +0400

    config types

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Slide from "./components/slide/slide";
 import Slider from "./components/slider/slider";
 
 const config = {
-  autoplay: true,
-  blockCount: 3,
-  navigation: true,
-  navigationPosition: "space-between",
-  pagination: true,
+  autoplay: true, // true || false
+  blockCount: 2, // 1 - children.length
+  navigation: true, // true || false
+  navigationPosition: "space-between", // center || space-between
+  pagination: true, // true || false
   loopTimer: 2000,
-  position: "center",
-  direction: "left",
-  paginationClickable: true,
+  position: "start", // start || center
+  direction: "right", // right || left
+  paginationClickable: true, // true || false
 };
 
 function App() {
